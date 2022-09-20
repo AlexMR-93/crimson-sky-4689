711aa5b77bd8e13167e9ee3376d8a6ea2f15b11b
Alex M <alexm0717@hotmail.com>
2022-09-20 15:15:26 -0700

    story 2 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -17,4 +17,18 @@ RSpec.describe("Dishes Show page") do
     expect(page).to(have_content("Ingredients:#{ingredient1.name}"))
     expect(page).to(have_content("Chefs name:#{chef1.name}"))
   end
+
+  #As a visitor
+  #When I visit a dish's show page
+  #I see the total calorie count for that dish.
+  it("I see the total calorie count for that dish.") do
+    chef1 = Chef.create!(    name: "Chef1")
+    dish1 = chef1.dishes.create!(    name: "hot dog",     description: "hot n ready",     chef_id: chef1.id)
+    ingredient1 = Ingredient.create!(    name: "Ketchup",     calories: 204)
+    ingredient2 = Ingredient.create!(    name: "Mustard",     calories: 206)
+    DishIngredient.create!(    dish_id: dish1.id,     ingredient_id: ingredient1.id)
+    DishIngredient.create!(    dish_id: dish1.id,     ingredient_id: ingredient2.id)
+    visit("/dishes/#{dish1.id}")
+    expect(page).to(have_content("Calorie Count:410"))
+  end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,11 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Dish, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :description}
+
+RSpec.describe(Dish, type: :model) do
+  describe("validations") do
+    it { should(validate_presence_of(:name)) }
+    it { should(validate_presence_of(:description)) }
   end
-  describe "relationships" do
-    it {should belong_to :chef}
+
+  describe("relationships") do
+    it { should(belong_to(:chef)) }
+  end
+
+  describe("model test sum") do
+    it("can add all calories together") do
+      chef1 = Chef.create!(      name: "Chef1")
+      dish1 = chef1.dishes.create!(      name: "hot dog",       description: "hot n ready",       chef_id: chef1.id)
+      ingredient1 = Ingredient.create!(      name: "Ketchup",       calories: 204)
+      ingredient2 = Ingredient.create!(      name: "Mustard",       calories: 206)
+      DishIngredient.create!(      dish_id: dish1.id,       ingredient_id: ingredient1.id)
+      DishIngredient.create!(      dish_id: dish1.id,       ingredient_id: ingredient2.id)
+      expect(dish1.cal_count).to(eq(410))
+    end
   end
 end
