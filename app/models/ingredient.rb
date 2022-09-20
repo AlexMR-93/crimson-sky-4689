e1ab05e35ebc2ca36c9541e7f82d95460a5ff9c4
Alex M <alexm0717@hotmail.com>
2022-09-20 14:18:56 -0700

    stroy 1 complete

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,4 @@
 class Ingredient < ApplicationRecord
-  belongs_to :chef
   has_many :dish_ingredients
   has_many :dishes,   through: :dish_ingredients
 end
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+#As a visitor
+#When I visit a dish's show page
+#I see the dish’s name and description
+#And I see a list of ingredients for that dish
+#And I see the chef's name.
+RSpec.describe("Dishes Show page") do
+  it("I see the dish’s name and description") do
+    chef1 = Chef.create!(    name: "Chef1")
+    dish1 = Dish.create!(    name: "hot dog",     description: "hot n ready",     chef_id: chef1.id)
+    ingredient1 = Ingredient.create!(    name: "Ketchup",     calories: 204)
+    DishIngredient.create!(    dish_id: dish1.id,     ingredient_id: ingredient1.id)
+    visit("/dishes/#{dish1.id}")
+    expect(page).to(have_content("Name:#{dish1.name}"))
+    expect(page).to(have_content("Description:#{dish1.description}"))
+    expect(page).to(have_content("Ingredients:#{ingredient1.name}"))
+    expect(page).to(have_content("Chefs name:#{chef1.name}"))
+  end
+end
